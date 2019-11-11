223a1467d667b1fc116aa46f8cb8bc52f6f00b18
Mario Baranzini <mario@opengis.ch>
2019-11-11 16:06:04 +0100

    Implement server app with REST user and group API

--- app/qfieldcloud/urls.py
+++ app/qfieldcloud/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 
+from rest_framework import routers
+from qfieldcloud.server import views
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('proba/', include('server.urls')),
+#    url(r'^api-auth/', include('rest_framework.urls')),
+#]
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
