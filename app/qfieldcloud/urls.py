ff82c29758eab9a72717069eb7460a9846fdefe1
Mario Baranzini <mario@opengis.ch>
2019-11-12 08:15:33 +0100

    Enable admin page

--- app/qfieldcloud/urls.py
+++ app/qfieldcloud/urls.py
@@ -20,13 +20,6 @@ from django.conf.urls import url
 from rest_framework import routers
 from qfieldcloud.server import views
 
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#    path('proba/', include('server.urls')),
-#    url(r'^api-auth/', include('rest_framework.urls')),
-#]
-
-
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -34,6 +27,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
