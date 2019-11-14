51113f2cd4806aad1ddd4716ad90354f23038999
Mario Baranzini <mario@opengis.ch>
2019-11-14 09:00:16 +0100

    Add token authentication

--- app/qfieldcloud/settings.py
+++ app/qfieldcloud/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'qfieldcloud.server',
 ]
 
@@ -126,9 +127,13 @@ STATIC_URL = '/static/'
 # Rest framework
 
 REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    #    'rest_framework.permissions.IsAuthenticated',
     ],
 }
