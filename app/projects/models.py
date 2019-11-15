46f042b7a28e74381a3439c327a843c5f3bc7a2e
Mario Baranzini <mario@opengis.ch>
2019-11-15 13:52:59 +0100

    Add project model

--- app/projects/models.py
+++ app/projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    """ Represent a QGIS Project """
+    name = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- app/projects/tests.py
+++ app/projects/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Project
+
+
+class ProjectTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        test_user1 = User.objects.create_user(
+            username='test_user1', password='abc123')
+        test_user1.save()
+
+        # Create a project
+        test_project = Project(
+            name='test_project', file_name='test_project.qgs', uploaded_by=test_user1)
+        test_project.save()
+
+    def test_project_content(self):
+        project = Project.objects.get(id=1)
+        self.assertEqual(project.name, 'test_project')
+        self.assertEqual(project.file_name, 'test_project.qgs')
+        self.assertEqual(str(project.uploaded_by), 'test_user1')
