144ef933fdd3922d13d435cb2618ccf275267c75
Mario Baranzini <mario@opengis.ch>
2019-11-15 16:23:23 +0100

    Fix prod settings

--- app/qfieldcloud_project/settings.py
+++ app/qfieldcloud_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g#n^=)(lx-0i#+t-*kix_6q^8%$*jir9)9b90!maes=%fpol56'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev.qfield.cloud']
 
 
 # Application definition
@@ -131,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
