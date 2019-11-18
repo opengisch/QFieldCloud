361501fc01f36688e25170f947f61065ff6a742a
Mario Baranzini <mario@opengis.ch>
2019-11-18 12:59:18 +0100

    Add final slash to push and pull API endpoints

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -6,6 +6,6 @@ from .views import ProjectView, ProjectFileView, PushView, PullView
 urlpatterns = [
     path('', ProjectView.as_view()),
     re_path(r'^(?P<project_name>[^/]+)$', ProjectFileView.as_view()),
-    re_path(r'^(?P<project_name>[^/]+)/push$', PushView.as_view()),
-    re_path(r'^(?P<project_name>[^/]+)/pull$', PullView.as_view()),
+    re_path(r'^(?P<project_name>[^/]+)/push/$', PushView.as_view()),
+    re_path(r'^(?P<project_name>[^/]+)/pull/$', PullView.as_view()),
 ]
