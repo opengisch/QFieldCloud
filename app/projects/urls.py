202a016040f89033a3b8b8d5357612a8f646dfa1
Mario Baranzini <mario@opengis.ch>
2019-11-18 16:56:15 +0100

    Refctor push view name

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, re_path
 
-from .views import ProjectView, ProjectFileView, PushView, PullView, FileUploadViewSet
+from .views import ProjectView, ProjectFileView, PushView, PullView
 
 
 urlpatterns = [
     path('', ProjectView.as_view()),
     re_path(r'^(?P<project_name>[^/]+)$', ProjectFileView.as_view()),
-    re_path(r'^(?P<project_name>[^/]+)/push/$', FileUploadViewSet.as_view()),
+    re_path(r'^(?P<project_name>[^/]+)/push/$', PushView.as_view()),
     re_path(r'^(?P<project_name>[^/]+)/pull/$', PullView.as_view()),
 ]
--- app/projects/views.py
+++ app/projects/views.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.models import User
 from rest_framework import generics, views, status
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -32,7 +30,8 @@ class ProjectFileView(generics.ListAPIView):
         )
 
 
-class FileUploadViewSet(generics.CreateAPIView):
+class PushView(generics.CreateAPIView):
+    """Push one or more files"""
 
     queryset = GenericFile.objects.all()
     serializer_class = GenericFileSerializer
@@ -50,24 +49,6 @@ class FileUploadViewSet(generics.CreateAPIView):
             g.save()
 
         return Response(status=status.HTTP_201_CREATED)
-
-
-class PushView(views.APIView):
-    """Push project"""
-
-    parser_classes = (MultiPartParser, FormParser,)
-
-    def post(self, request, project_name):
-        # return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
-        print("########", request.FILES)
-        my_file = request.FILES['file']
-        filename = '/tmp/myfile'
-        with open(filename, 'wb+') as temp_file:
-            for chunk in my_file.chunks():
-                temp_file.write(chunk)
-
-        # my_saved_file = open(filename)
-        return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 
 class PullView(views.APIView):
