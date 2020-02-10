cac4447f2e2cd00d984ed27c4efaa78616b63d5d
Mario Baranzini <mario@opengis.ch>
2020-02-10 11:07:04 +0100

    Implement project roles permissions

--- web-app/qfieldcloud/apps/api/permissions.py
+++ web-app/qfieldcloud/apps/api/permissions.py
@@ -1,57 +1,116 @@
-from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import get_user_model
 
 from rest_framework import permissions
 
 from qfieldcloud.apps.model.models import (
-    Project,
-    #ProjectRole
+    Project, ProjectCollaborator
 )
+
 
 class IsProjectOwner(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        print("has permission: {} {}".format(request, view))
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
-        print("isProjectOwner: {} {} {}".format(request, view, obj))
-        return True
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+        except get_user_model().DoesNotExist:
+            return False
+        return request.user == owner
 
 
 class IsProjectAdmin(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
+    def has_permission(self, request, view):
+        request_project = request.parser_context['kwargs']['project']
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+            project = Project.objects.get(name=request_project, owner=owner)
+
+            ProjectCollaborator.objects.get(
+                project=project, collaborator=request.user,
+                role=ProjectCollaborator.ROLE_ADMIN
+            )
+        except ObjectDoesNotExist:
+            return False
+
         return True
 
 
-class IsProjectManagerOrHigher(permissions.BasePermission):
+class IsProjectManager(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
+    def has_permission(self, request, view):
+        request_project = request.parser_context['kwargs']['project']
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+            project = Project.objects.get(name=request_project, owner=owner)
+
+            ProjectCollaborator.objects.get(
+                project=project, collaborator=request.user,
+                role=ProjectCollaborator.ROLE_MANAGER
+            )
+        except ObjectDoesNotExist:
+            return False
+
         return True
 
 
-class IsProjectEditorOrHigher(permissions.BasePermission):
+class IsProjectEditor(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
+    def has_permission(self, request, view):
+        request_project = request.parser_context['kwargs']['project']
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+            project = Project.objects.get(name=request_project, owner=owner)
+
+            ProjectCollaborator.objects.get(
+                project=project, collaborator=request.user,
+                role=ProjectCollaborator.ROLE_EDITOR
+            )
+        except ObjectDoesNotExist:
+            return False
+
         return True
 
 
-class IsProjectReporterOrHigher(permissions.BasePermission):
+class IsProjectReporter(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
+    def has_permission(self, request, view):
+        request_project = request.parser_context['kwargs']['project']
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+            project = Project.objects.get(name=request_project, owner=owner)
+
+            ProjectCollaborator.objects.get(
+                project=project, collaborator=request.user,
+                role=ProjectCollaborator.ROLE_REPORTER
+            )
+        except ObjectDoesNotExist:
+            return False
+
         return True
 
 
-class IsProjectReaderOrHigher(permissions.BasePermission):
+class IsProjectReader(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        # TODO: implement
+    def has_permission(self, request, view):
+        request_project = request.parser_context['kwargs']['project']
+        request_owner = request.parser_context['kwargs']['owner']
+        try:
+            owner = get_user_model().objects.get(username=request_owner)
+            project = Project.objects.get(name=request_project, owner=owner)
+
+            ProjectCollaborator.objects.get(
+                project=project, collaborator=request.user,
+                role=ProjectCollaborator.ROLE_READER
+            )
+        except ObjectDoesNotExist:
+            return False
+
         return True
 
 
@@ -62,147 +121,8 @@ class IsOrganizationAdmin(permissions.BasePermission):
         return True
 
 
-class IsOrganizationMemberOrHigher(permissions.BasePermission):
+class IsOrganizationMember(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # TODO: implement
         return True
-
-
-def get_key_from_value(value):
-    """Return the key from the role value
-    e.g. return 'admin' for 1"""
-
-    return dict(zip(settings.PROJECT_ROLE.values(),
-                    settings.PROJECT_ROLE.keys()))[value]
-
-
-def is_project_owner(username, project_name):
-    """Return True if the user is the owner of the project
-    otherwise return False"""
-
-    project = Project.objects.get(name=project_name)
-    user = get_user_model().objects.get(username=username)
-
-    return project.owner == user
-
-
-def is_project_admin(username, project_name):
-    """Return True if the user is admin or owner of the project
-    otherwise return False"""
-
-    if is_project_owner(username, project_name):
-        return True
-
-    project = Project.objects.get(name=project_name)
-    user = get_user_model().objects.get(username=username)
-
-    #if ProjectRole.objects.filter(
-    #        user=user, project=project,
-    #        role=settings.PROJECT_ROLE['admin']):
-    #    return True
-
-    return False
-
-
-def is_project_manager(username, project_name):
-    """Return True if user is manager, admin or own the project
-    otherwise return False"""
-
-    if is_project_owner(username, project_name):
-        return True
-
-    if is_project_admin(username, project_name):
-        return True
-
-    project = Project.objects.get(name=project_name)
-    user = get_user_model().objects.get(username=username)
-
-    #if ProjectRole.objects.filter(
-    #        user=user, project=project,
-    #        role=settings.PROJECT_ROLE['manager']):
-    #    return True
-
-    return False
-
-
-def is_project_editor(username, project_name):
-    """Return True if user is editor, manager, admin or own the project
-    otherwise return False"""
-
-    if is_project_owner(username, project_name):
-        return True
-
-    if is_project_admin(username, project_name):
-        return True
-
-    if is_project_manager(username, project_name):
-        return True
-
-    project = Project.objects.get(name=project_name)
-    user = get_user_model().objects.get(username=username)
-
-    #if ProjectRole.objects.filter(
-    #        user=user, project=project,
-    #        role=settings.PROJECT_ROLE['editor']):
-    #    return True
-
-    return False
-
-
-def is_project_reporter(username, project_name):
-    """Return True id user is reporter, editor, manager, admin or own the project
-    otherwise return False"""
-
-    if is_project_owner(username, project_name):
-        return True
-
-    if is_project_admin(username, project_name):
-        return True
-
-    if is_project_manager(username, project_name):
-        return True
-
-    if is_project_editor(username, project_name):
-        return True
-
-    project = Project.objects.get(name=project_name)
-    user = get_user_model().objects.get(username=username)
-
-    #if ProjectRole.objects.filter(
-    #        user=user, project=project,
-    #        role=settings.PROJECT_ROLE['reporter']):
-    #    return True
-
-    return False
-
-
-def is_project_reader(username, project_name):
-    """Return True if user is reader, reporter, editor, manager, admin,
-    own the project or the project is not private otherwise return False"""
-
-    project = Project.objects.get(name=project_name)
-
-    if not project.private:
-        return True
-
-    if is_project_owner(username, project_name):
-        return True
-
-    if is_project_admin(username, project_name):
-        return True
-
-    if is_project_manager(username, project_name):
-        return True
-
-    if is_project_editor(username, project_name):
-        return True
-
-    user = get_user_model().objects.get(username=username)
-
-    #if ProjectRole.objects.filter(
-    #        user=user, project=project,
-    #        role=settings.PROJECT_ROLE['reader']):
-    #    return True
-
-    return False
