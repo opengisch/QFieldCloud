671d36efcb7a73b4ed263212e0ac1eb4a7f8b69c
Mario Baranzini <mario@opengis.ch>
2020-02-11 07:08:37 +0100

    Change urls order to avoid ambiguity

--- web-app/qfieldcloud/apps/api/urls.py
+++ web-app/qfieldcloud/apps/api/urls.py
@@ -17,22 +17,22 @@ from .views import (
 
 
 urlpatterns = [
+    path('users/user/', RetrieveUpdateAuthenticatedUserView.as_view()),
     path('users/', ListUsersView.as_view()),
     path('users/<str:name>/', RetrieveUserView.as_view()),
-    path('users/user/', RetrieveUpdateAuthenticatedUserView.as_view()),
 
-    path('projects/', ListProjectsView.as_view()),
     path('projects/user/', ListUserProjectsView.as_view()),
+    path('projects/', ListProjectsView.as_view()),
     path('projects/<str:owner>/', ListCreateProjectView.as_view()),
-    path('projects/<str:owner>/<str:project>/',
-         RetrieveUpdateDestroyProjectView.as_view()),
     path('projects/<str:owner>/<str:project>/push/', PushFileView.as_view()),
-
     path('projects/<str:owner>/<str:project>/files/', ListFilesView.as_view()),
     path('projects/<str:owner>/<str:project>/collaborators/',
          ListCollaboratorsView.as_view()),
     path('projects/<str:owner>/<str:project>/collaborators/<str:username>/',
          CheckCreateDestroyCollaboratorView.as_view()),
+    path('projects/<str:owner>/<str:project>/',
+         RetrieveUpdateDestroyProjectView.as_view()),
     path('projects/<str:owner>/<str:project>/<path:filename>/',
          RetrieveDestroyFileView.as_view()),
+
 ]
