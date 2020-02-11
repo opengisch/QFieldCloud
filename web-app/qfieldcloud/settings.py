41022bf141242d704bce52d9bbf2ff8c1a2a2791
Mario Baranzini <mario@opengis.ch>
2020-02-11 09:34:19 +0100

    Use postgres instead of sqlite

--- web-app/qfieldcloud/settings.py
+++ web-app/qfieldcloud/settings.py
@@ -96,6 +96,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
