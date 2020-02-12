4377d67a99ed8b6680276cbf4585cbac18439b37
Mario Baranzini <mario@opengis.ch>
2020-02-12 10:52:31 +0100

    Replace the deprecated rest_framework_swagger with drf_yasg
    using redoc

--- web-app/qfieldcloud/settings.py
+++ web-app/qfieldcloud/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
+    'drf_yasg',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
--- web-app/qfieldcloud/urls.py
+++ web-app/qfieldcloud/urls.py
@@ -14,14 +14,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from django.urls import path, re_path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="QFieldcloud REST API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://",
+      contact=openapi.Contact(email="info@opengis.ch"),
+      license=openapi.License(name="License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('docs/',
+         schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('api/v1/auth/registration/', include('rest_auth.registration.urls')),
     path('api/v1/auth/', include('rest_auth.urls')),
     path('api/v1/', include('qfieldcloud.apps.api.urls')),
-    path('docs/', get_swagger_view(title='QFieldcloud REST API')),
     path('auth/', include('rest_framework.urls')),
 ]
