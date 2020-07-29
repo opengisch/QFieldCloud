ac0c581c654e46006c2a58a4ecb6cf11095039a2
Mario Baranzini <mario@opengis.ch>
2020-07-29 09:33:29 +0200

    Change authentication classes oreder

--- web-app/qfieldcloud/settings.py
+++ web-app/qfieldcloud/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
