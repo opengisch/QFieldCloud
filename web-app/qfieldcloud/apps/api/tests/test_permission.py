7307a0d1d43dd59e849d781a95419c0b759a7ab5
Mario Baranzini <mario@opengis.ch>
2020-07-29 13:03:17 +0200

    Fix test return code check

--- web-app/qfieldcloud/apps/api/tests/test_permission.py
+++ web-app/qfieldcloud/apps/api/tests/test_permission.py
@@ -113,7 +113,7 @@ class PermissionTestCase(APITransactionTestCase):
         response = self.client.get(
             '/api/v1/files/{}/foo/bar/file.txt/?client=qgis'.format(self.project1.id))
         self.assertFalse(status.is_success(response.status_code))
-        self.assertEqual(response.status_code, 403)
+        self.assertTrue(response.status_code.isClientError())
 
     def test_project_admin_can_create_collaborator(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token2.key)
