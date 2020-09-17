fa0a06d9a42c6ab443f62a5eb96dff474c87fe14
Mario Baranzini <mario@opengis.ch>
2020-09-17 17:02:07 +0200

    Cache the result of the status api for 10 minutes

--- django-app/qfieldcloud/core/tests/test_api.py
+++ django-app/qfieldcloud/core/tests/test_api.py
@@ -1,4 +1,7 @@
+import time
+
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 
 from rest_framework import status
 from rest_framework.test import APITransactionTestCase
@@ -8,9 +11,23 @@ User = get_user_model()
 
 class StatusTestCase(APITransactionTestCase):
 
+    def setUp(self):
+        # Empty cache value
+        cache.delete('status_results')
+
     def test_api_status(self):
         response = self.client.get('/api/v1/status/')
         self.assertTrue(status.is_success(response.status_code))
         self.assertIn(response.json()['orchestrator'], ['ok', 'slow'])
         self.assertEqual(response.json()['redis'], 'ok')
         self.assertEqual(response.json()['storage'], 'ok')
+
+    def test_api_status_cache(self):
+        tic = time.perf_counter()
+        self.client.get('/api/v1/status/')
+        toc = time.perf_counter()
+        self.client.get('/api/v1/status/')
+        tac = time.perf_counter()
+
+        self.assertGreater(toc - tic, 5)
+        self.assertLess(tac - toc, 1)
--- django-app/qfieldcloud/core/views/status_views.py
+++ django-app/qfieldcloud/core/views/status_views.py
@@ -2,6 +2,7 @@ import time
 
 from django.utils.decorators import method_decorator
 from django.conf import settings
+from django.core.cache import cache
 
 from rest_framework import views, status
 from rest_framework.response import Response
@@ -21,37 +22,42 @@ class APIStatusView(views.APIView):
 
     def get(self, request):
 
-        results = {}
+        # Try to get the status from the cache
+        results = cache.get('status_results', {})
+        if not results:
 
-        results['redis'] = 'ok'
-        # Check if redis is visible
-        if not utils.redis_is_running():
-            results['redis'] = 'error'
+            results['redis'] = 'ok'
+            # Check if redis is visible
+            if not utils.redis_is_running():
+                results['redis'] = 'error'
 
-        results['storage'] = 'ok'
-        # Check if bucket exists (i.e. the connection works)
-        try:
-            s3_client = utils.get_s3_client()
-            s3_client.head_bucket(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
-        except Exception:
-            results['storage'] = 'error'
+            results['storage'] = 'ok'
+            # Check if bucket exists (i.e. the connection works)
+            try:
+                s3_client = utils.get_s3_client()
+                s3_client.head_bucket(Bucket=settings.AWS_STORAGE_BUCKET_NAME)
+            except Exception:
+                results['storage'] = 'error'
 
-        job = utils.check_orchestrator_status()
-        results['orchestrator'] = 'ok'
-        # Wait for the worker to finish
-        for _ in range(30):
-            time.sleep(2)
-            if job.get_status() == 'finished':
-                if _ >= 10:
-                    results['orchestrator'] = 'slow'
-                else:
-                    results['orchestrator'] = 'ok'
-                break
-            if job.get_status() == 'failed':
-                break
+            job = utils.check_orchestrator_status()
+            results['orchestrator'] = 'ok'
+            # Wait for the worker to finish
+            for _ in range(30):
+                time.sleep(2)
+                if job.get_status() == 'finished':
+                    if _ >= 10:
+                        results['orchestrator'] = 'slow'
+                    else:
+                        results['orchestrator'] = 'ok'
+                    break
+                if job.get_status() == 'failed':
+                    break
 
-        if not job.get_status() in ['finished']:
-            results['orchestrator'] = 'error'
+            if not job.get_status() in ['finished']:
+                results['orchestrator'] = 'error'
+
+            # Cache the result for 10 minutes
+            cache.set('status_results', results, 600)
 
         for result in results:
             if not results[result] in ['slow', 'ok']:
