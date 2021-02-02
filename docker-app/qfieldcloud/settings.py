eb2f9e70132b9caf4e81eb0c217024318fb359b7
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-02-03 00:32:36 +0200

    Failed at adding context_processors

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -111,6 +111,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'qfieldcloud.core.web.context_processors',
             ],
         },
     },
