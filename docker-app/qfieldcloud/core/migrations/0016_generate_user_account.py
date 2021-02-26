9e73c032dfbea7cf8fcda45310eb0c924d7f659a
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-02-26 19:47:50 +0200

    Generate user account migration

---
+++ docker-app/qfieldcloud/core/migrations/0016_generate_user_account.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+def generate_user_account(apps, schema_editor):
+    User = apps.get_model('core', 'User')
+    UserAccount = apps.get_model('core', 'UserAccount')
+
+    for user in User.objects.filter(useraccount=None):
+        UserAccount.objects.create(user=user)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0015_auto_20210123_0116'),
+    ]
+
+    operations = [
+        migrations.RunPython(generate_user_account, reverse_code=migrations.RunPython.noop),
+    ]
