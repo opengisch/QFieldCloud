2d50e6ef470d65e253c9e503781e4487f9ec8958
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-03-05 15:56:15 +0200

    e-mail -> email

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -36,7 +36,7 @@ AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
-    # `allauth` specific authentication methods, such as login by e-mail
+    # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
