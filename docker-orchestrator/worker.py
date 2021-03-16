e1f6f8d49dcff78148ee98e86be1c9b65109d91d
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-03-16 23:11:41 +0200

    PIP8 worker

--- docker-orchestrator/worker.py
+++ docker-orchestrator/worker.py
@@ -1,45 +1,43 @@
-import os
 import logging
+import os
 from logging import handlers
 
 import sentry_sdk
+from redis import Redis
+from rq import Connection, Worker
 from sentry_sdk.integrations.rq import RqIntegration
 
-from rq import Connection, Worker
-from redis import Redis
-
-
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN', ''),
+    dsn=os.environ.get("SENTRY_DSN", ""),
     integrations=[RqIntegration()],
-    server_name=os.environ.get('QFIELDCLOUD_HOST'),
-    attach_stacktrace='on',
+    server_name=os.environ.get("QFIELDCLOUD_HOST"),
+    attach_stacktrace="on",
 )
 
 
 rotating_file_handler = handlers.RotatingFileHandler(
-    filename=os.path.join('/log', 'orchestrator.log'),
-    mode='a',
+    filename=os.path.join("/log", "orchestrator.log"),
+    mode="a",
     maxBytes=5 * 1024 * 1024,
     backupCount=2,
     encoding=None,
-    delay=False
+    delay=False,
 )
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s %(message)s',
+    format="%(asctime)s %(message)s",
     handlers=[rotating_file_handler],
 )
 
 with Connection():
     redis = Redis(
-        host=os.environ.get('REDIS_HOST'),
-        password=os.environ.get('REDIS_PASSWORD'),
-        port=os.environ.get('REDIS_PORT')
+        host=os.environ.get("REDIS_HOST"),
+        password=os.environ.get("REDIS_PASSWORD"),
+        port=os.environ.get("REDIS_PORT"),
     )
 
-    qs = ['delta', 'export']
+    qs = ["delta", "export"]
 
     w = Worker(qs, connection=redis)
     w.work()
