87bbacf0efe69703f7cb472f3e15c1faa9ae5bb5
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-05-13 02:26:43 +0300

    Apply all deltas that have no currently active job for them

--- docker-app/qfieldcloud/core/utils2/jobs.py
+++ docker-app/qfieldcloud/core/utils2/jobs.py
@@ -15,10 +15,22 @@ def apply_deltas(
         f"Requested apply_deltas on {project} with {project_file}; overwrite_conflicts: {overwrite_conflicts}; delta_ids: {delta_ids}"
     )
 
+    queue = django_rq.get_queue("delta")
+    job_ids = queue.started_job_registry.get_job_ids()
+
     delta_apply_job = ApplyJob.objects.create(
         project=project, created_by=user, overwrite_conflicts=overwrite_conflicts
     )
-    pending_deltas = Delta.objects.filter(last_status__in=[Delta.Status.PENDING])
+    pending_deltas = Delta.objects.filter(
+        last_status__in=[
+            # do not include deltas with NOT_APPLIED status, as it is a final status
+            Delta.Status.PENDING,
+            Delta.Status.STARTED,
+            Delta.Status.ERROR,
+        ],
+    ).exclude(
+        id__in=ApplyJobDelta.objects.filter(delta_apply_job_id__in=job_ids).values("delta_id")
+    )
 
     if delta_ids is not None:
         pending_deltas = pending_deltas.filter(pk__in=delta_ids)
@@ -30,7 +42,6 @@ def apply_deltas(
         delta_apply_job.deltas_to_apply.add(delta)
 
     job_id = delta_apply_job.id
-    queue = django_rq.get_queue("delta")
     queue.enqueue(
         "orchestrator.apply_deltas", str(job_id), str(project_file), job_id=str(job_id)
     )
