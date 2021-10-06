e626c35828855fd4e203c6e9bab521a4553c6303
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-10-06 16:39:15 +0300

    Fix validators showing multiple validation errors for the same thing

--- docker-app/qfieldcloud/core/validators.py
+++ docker-app/qfieldcloud/core/validators.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
+from django.utils.translation import gettext as _
 
 
 def reserved_words_validator(value):
@@ -29,17 +29,4 @@ def reserved_words_validator(value):
         "esri",
     ]
     if value.lower() in reserved_words:
-        raise ValidationError('"{}" is a reserved word!'.format(value))
-
-
-allowed_symbols_validator = RegexValidator(
-    r"^[-a-zA-Z0-9_]+$", "Only letters, numbers, underscores or hyphens are allowed."
-)
-
-min_lenght_validator = RegexValidator(
-    r"^.{3,}$", "The name must be at least 3 characters long."
-)
-
-first_symbol_validator = RegexValidator(
-    r"^[a-zA-Z].*$", "The name must begin with a letter."
-)
+        raise ValidationError(_('"{}" is a reserved word!').format(value))
