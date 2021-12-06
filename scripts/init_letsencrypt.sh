07cd97076f3dc26e79cd6832c3a6a20c350312ba
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-12-06 03:34:29 +0200

    nginx template

--- scripts/init_letsencrypt.sh
+++ scripts/init_letsencrypt.sh
@@ -7,43 +7,36 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dev.qfield.cloud)
-rsa_key_size=4096
-config_path="./conf/nginx"
-email="info@opengis.ch" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+set -a; source .env; set +a
 
-if [ ! -e "$config_path/options-ssl-nginx.conf" ] || [ ! -e "$config_path/ssl-dhparams.pem" ]; then
+CONFIG_PATH="./conf/nginx"
+
+if [ ! -e "$CONFIG_PATH/options-ssl-nginx.conf" ] || [ ! -e "$CONFIG_PATH/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$config_path/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$config_path/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$CONFIG_PATH/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$CONFIG_PATH/ssl-dhparams.pem"
   echo
 fi
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
-
-exit
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+echo "### Requesting Let's Encrypt certificate for $QFIELDCLOUD_HOST ..."
+domain_args="-d ${QFIELDCLOUD_HOST}"
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ $LETSENCRYPT_STAGING != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $domain_args \
-    --email $email \
-    --rsa-key-size $rsa_key_size \
+    --email $LETSENCRYPT_EMAIL \
+    --rsa-key-size $LETSENCRYPT_RSA_KEY_SIZE \
     --agree-tos \
     --force-renewal" certbot
+echo
+
+echo "### Copy the certificate and key to their final destination ..."
+cp conf/certbot/conf/live/${QFIELDCLOUD_HOST}/fullchain.pem conf/nginx/certs/${QFIELDCLOUD_HOST}.pem
+cp conf/certbot/conf/live/${QFIELDCLOUD_HOST}/privkey.pem conf/nginx/certs/${QFIELDCLOUD_HOST}-key.pem
 echo
 
 echo "### Reloading nginx ..."
