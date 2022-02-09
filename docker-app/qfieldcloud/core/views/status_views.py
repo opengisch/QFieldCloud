984797ec94ca82881a2ee63e0b1363b912c9cba0
olivierdalang <olivier.dalang@gmail.com>
2022-02-09 12:04:51 +0100

    drop request logging logic altogether
    (can now be done at nginx level)

--- docker-app/qfieldcloud/core/views/status_views.py
+++ docker-app/qfieldcloud/core/views/status_views.py
@@ -3,7 +3,6 @@ from django.core.cache import cache
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from qfieldcloud.core import geodb_utils, utils
-from qfieldcloud.core.logging.filters import skip_logging
 from rest_framework import status, views
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -19,7 +18,6 @@ from rest_framework.response import Response
 class APIStatusView(views.APIView):
     permission_classes = [AllowAny]
 
-    @skip_logging
     def get(self, request):
         # Try to get the status from the cache
         results = cache.get("status_results", {})
