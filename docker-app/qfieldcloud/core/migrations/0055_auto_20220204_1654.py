c1d8bfec12b7572abee1f2e69effd4b77329600f
olivierdalang <olivier.dalang@gmail.com>
2022-05-09 15:24:00 +0200

    linearize migrations

--- docker-app/qfieldcloud/core/migrations/0055_auto_20220204_1654.py
+++ docker-app/qfieldcloud/core/migrations/0055_auto_20220204_1654.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("subscription", "0002_populate_account_types"),
-        ("core", "0052_secret"),
+        ("core", "0053_project_last_package.py"),
     ]
 
     operations = [
