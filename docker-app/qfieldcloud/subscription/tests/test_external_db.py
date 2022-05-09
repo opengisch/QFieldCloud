7f6361e1c3e7c788d299bf6f3098b094d2a9153d
olivierdalang <olivier.dalang@gmail.com>
2022-05-09 14:23:59 +0200

    improve test

--- docker-app/qfieldcloud/subscription/tests/test_external_db.py
+++ docker-app/qfieldcloud/subscription/tests/test_external_db.py
@@ -17,7 +17,7 @@ class QfcTestCase(APITransactionTestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
 
     def _wait(self, project, job_type, expected_result=Job.Status.FINISHED):
-        print(f"Waiting for {job_type} status {expected_result}", end="")
+        # print(f"Waiting for {job_type} status {expected_result}", end="")
         for _ in range(30):
             job = (
                 Job.objects.filter(project=project, type=job_type)
@@ -32,13 +32,13 @@ class QfcTestCase(APITransactionTestCase):
             )
             if job:
                 if job.status == expected_result:
-                    print(" done")
+                    # print(" done")
                     break
                 else:
-                    print(f" got unexpeced result ! ({job.status})")
+                    print(f" got unexpected result ! ({job.status})")
                     print(job.feedback)
                     raise Exception("Unexpected result failed !")
-            print(".", end="")
+            # print(".", end="")
             sleep(1)
         else:
             raise Exception("Processing did no finish, did the worker hang ?")
@@ -69,6 +69,29 @@ class QfcTestCase(APITransactionTestCase):
         )
         self.assertTrue(response.status_code, 201)
         self._wait(p1, Job.Type.PACKAGE)
+
+        # Test various endpoints
+        for requests_to_check in [
+            # TODO: list all endpoints that we wish would fail here
+            ["GET", f"/api/v1/packages/{p1.pk}/latest/"],
+            ["GET", f"/api/v1/packages/{p1.pk}/latest/files/project.qgs/"],
+            # ["GET", f"/api/v1/deltas/{p1.pk}/"],
+        ]:
+            # If account type has external db permission, endpoint works
+            AccountType.objects.all().update(is_external_db_supported=True)
+            response = self.client.generic(*requests_to_check)
+            self.assertTrue(
+                status.is_success(response.status_code),
+                f"Unexpected failure: {requests_to_check}",
+            )
+
+            # If account type has not external db permission, endpoint fails
+            AccountType.objects.all().update(is_external_db_supported=False)
+            response = self.client.generic(*requests_to_check)
+            self.assertFalse(
+                status.is_success(response.status_code),
+                f"Unexpected success: {requests_to_check}",
+            )
 
     def test_is_external_db_notsupported(self):
         """This tests is_external_db_supported property of accounts types"""
