9baaa7a6a34ddf6f77f55cb2cf8c1b18dea69a24
Ivan Ivanov <ivan.ivanov@suricactus.com>
2022-05-10 13:51:55 +0300

    Fix migrations

--- docker-app/qfieldcloud/core/migrations/0055_auto_20220506_1240.py
+++ docker-app/qfieldcloud/core/migrations/0055_auto_20220506_1240.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0053_project_last_package"),
+        ("core", "0054_auto_20220505_1948"),
     ]
 
     operations = [
