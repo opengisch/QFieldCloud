4ded8af8343a1c9589bcb6f9db28e811eca4b098
olivierdalang <olivier.dalang@gmail.com>
2022-08-25 09:30:29 +0200

    empty content exception wrongly reportes as server error
    should fix sentry issue 3528247758

--- docker-app/qfieldcloud/core/exceptions.py
+++ docker-app/qfieldcloud/core/exceptions.py
@@ -84,7 +84,7 @@ class EmptyContentError(QFieldCloudException):
 
     code = "empty_content"
     message = "Empty content"
-    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+    status_code = status.HTTP_400_BAD_REQUEST
 
 
 class MultipleContentsError(QFieldCloudException):
