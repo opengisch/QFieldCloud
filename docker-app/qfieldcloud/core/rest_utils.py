65db52221c2492729a27bcc22e3b737182e2411c
olivierdalang <olivier.dalang@gmail.com>
2022-08-25 09:29:50 +0200

    minor logging refactoring
    (we reuse the http code to determine if it's sever error or client error)

--- docker-app/qfieldcloud/core/rest_utils.py
+++ docker-app/qfieldcloud/core/rest_utils.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.core import exceptions
 from qfieldcloud.core import exceptions as qfieldcloud_exceptions
 from rest_framework import exceptions as rest_exceptions
+from rest_framework import status
 from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
@@ -12,7 +13,6 @@ logger = logging.getLogger(__name__)
 def exception_handler(exc, context):
 
     # Map exceptions to qfc exceptions
-    is_error = False
     if isinstance(exc, rest_exceptions.AuthenticationFailed):
         qfc_exc = qfieldcloud_exceptions.AuthenticationFailedError()
     elif isinstance(exc, rest_exceptions.NotAuthenticated):
@@ -24,19 +24,17 @@ def exception_handler(exc, context):
     elif isinstance(exc, exceptions.ValidationError):
         qfc_exc = qfieldcloud_exceptions.ValidationError(detail=str(exc))
     elif isinstance(exc, qfieldcloud_exceptions.QFieldCloudException):
-        is_error = True
         qfc_exc = exc
     elif isinstance(exc, rest_exceptions.APIException):
-        is_error = True
         qfc_exc = qfieldcloud_exceptions.APIError(exc.detail, exc.status_code)
     else:
         # Unexpected ! We rethrow original exception to make debugging tests easier
         if settings.IN_TEST_SUITE:
             raise exc
-        is_error = True
         qfc_exc = qfieldcloud_exceptions.QFieldCloudException(detail=str(exc))
 
-    if is_error:
+    # Log level depends on the status code (5xx are logged as errors, 4xx are logged in info)
+    if not status.is_client_error(qfc_exc.status_code):
         # log the original exception
         logging.exception(exc)
     else:
