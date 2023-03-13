10e44e374c3dae6b33acc6517927c288cc7d444f
Ivan Ivanov <ivan.ivanov@suricactus.com>
2023-03-13 14:09:48 +0200

    Silent `QuotaError` from Sentry

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -265,12 +265,13 @@ if SENTRY_DSN:
     SENTRY_SAMPLE_RATE = float(os.environ.get("SENTRY_SAMPLE_RATE", 1))
 
     def before_send(event, hint):
-        from qfieldcloud.core.exceptions import ProjectAlreadyExistsError
+        from qfieldcloud.core.exceptions import ProjectAlreadyExistsError, QuotaError
         from rest_framework.exceptions import ValidationError
 
         ignored_exceptions = (
             ValidationError,
             ProjectAlreadyExistsError,
+            QuotaError,
         )
 
         if "exc_info" in hint:
