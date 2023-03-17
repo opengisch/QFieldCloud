c452358ca0963d866c917ac3df5b413fb87882c0
faebebin <fabianbndr@gmail.com>
2023-03-17 13:14:46 +0100

    Remove unnecessary try exept

--- docker-app/qfieldcloud/core/paginators.py
+++ docker-app/qfieldcloud/core/paginators.py
@@ -10,6 +10,7 @@ class LargeTablePaginator(Paginator):
     """
     Only for Postgres:
     Overrides the count method to get an estimate instead of actual count when not filtered
+    inspired by: https://djangosnippets.org/snippets/2855/
     """
 
     EXACT_COUNT_LIMIT = 10000
@@ -22,25 +23,19 @@ class LargeTablePaginator(Paginator):
         """
         c = getattr(self.object_list, "count", None)
         if callable(c) and not inspect.isbuiltin(c) and method_has_no_args(c):
-            try:
-                estimate = 0
-                if not self.object_list.query.where:
-                    try:
-                        cursor = connection.cursor()
-                        cursor.execute(
-                            "SELECT reltuples FROM pg_class WHERE relname = %s",
-                            [self.object_list.query.model._meta.db_table],
-                        )
-                        estimate = int(cursor.fetchone()[0])
-                    except:  # noqa: E722
-                        pass
-                if estimate < self.EXACT_COUNT_LIMIT:
-                    return c()
-                else:
-                    return estimate
-            except (AttributeError, TypeError):
-                # AttributeError if object_list has no count() method.
-                # TypeError if object_list.count() requires arguments
-                # (i.e. is of type list).
-                pass
+            estimate = 0
+            if not self.object_list.query.where:
+                try:
+                    cursor = connection.cursor()
+                    cursor.execute(
+                        "SELECT reltuples FROM pg_class WHERE relname = %s",
+                        [self.object_list.query.model._meta.db_table],
+                    )
+                    estimate = int(cursor.fetchone()[0])
+                except:  # noqa: E722
+                    pass
+            if estimate < self.EXACT_COUNT_LIMIT:
+                return c()
+            else:
+                return estimate
         return len(self.object_list)
