5afbdbb0b4416a803a38551fb8cd7b84e5e53f2c
faebebin <fabianbndr@gmail.com>
2023-05-03 14:20:48 +0200

    temp comment (drop)

--- docker-app/qfieldcloud/subscription/exceptions.py
+++ docker-app/qfieldcloud/subscription/exceptions.py
@@ -1,6 +1,21 @@
+from rest_framework import status
+from core.exceptions import QFieldCloudException
+
 class SubscriptionException(Exception):
     ...
 
 
-class NotPremiumPlanException(SubscriptionException):
-    ...
+class NotPremiumPlanException(QFieldCloudException):
+    """Raised when TODO ..."""
+
+    # TODO code = ""
+    # message = ""
+    status_code = status.HTTP_403_FORBIDDEN
+
+
+# TODO Rather put here? (from core.exceptions)
+# class SubscriptionInactiveError(QFieldCloudException):
+# 
+# class ReachedMaxOrganizationMembersError(QFieldCloudException):
+# 
+# class QuotaError(QFieldCloudException):
