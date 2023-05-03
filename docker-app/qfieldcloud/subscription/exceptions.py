0a45eadbfcf8569716ce66e09022dc264a89925c
faebebin <fabianbndr@gmail.com>
2023-05-03 14:39:04 +0200

    Revert "temp comment (drop)"
    This reverts commit 5afbdbb0b4416a803a38551fb8cd7b84e5e53f2c.

--- docker-app/qfieldcloud/subscription/exceptions.py
+++ docker-app/qfieldcloud/subscription/exceptions.py
@@ -1,21 +1,6 @@
-from rest_framework import status
-from core.exceptions import QFieldCloudException
-
 class SubscriptionException(Exception):
     ...
 
 
-class NotPremiumPlanException(QFieldCloudException):
-    """Raised when TODO ..."""
-
-    # TODO code = ""
-    # message = ""
-    status_code = status.HTTP_403_FORBIDDEN
-
-
-# TODO Rather put here? (from core.exceptions)
-# class SubscriptionInactiveError(QFieldCloudException):
-# 
-# class ReachedMaxOrganizationMembersError(QFieldCloudException):
-# 
-# class QuotaError(QFieldCloudException):
+class NotPremiumPlanException(SubscriptionException):
+    ...
