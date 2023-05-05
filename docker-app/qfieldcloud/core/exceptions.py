0c04f42dc6d1ec488b0ce5afc92cd2bf3cc038b0
faebebin <fabianbndr@gmail.com>
2023-05-05 15:18:48 +0200

    Review correction: remove instead of deprectation

--- docker-app/qfieldcloud/core/exceptions.py
+++ docker-app/qfieldcloud/core/exceptions.py
@@ -169,34 +169,3 @@ class ReachedMaxOrganizationMembersError(QFieldCloudException):
     code = "organization_has_max_number_of_members"
     message = "Cannot add new organization members, account limit has been reached."
     status_code = status.HTTP_403_FORBIDDEN
-
-
-@deprecated("moved to subscription")
-class InactiveSubscriptionError(QFieldCloudException):
-    """
-    Raised when a permission (action) is denied because the useraccount subscription is inactive.
-    """
-
-    code = "inactive_subscription"
-    message = "Permission denied because the subscription is inactive"
-    status_code = status.HTTP_403_FORBIDDEN
-
-
-@deprecated("moved to subscription")
-class PlanInsufficientError(QFieldCloudException):
-    """
-    Raised when a permission (action) is denied because the useraccount plan is insufficient.
-    """
-
-    code = "permission_denied_plan_insufficient"
-    message = "Permission denied because the useraccount's plan is insufficient"
-    status_code = status.HTTP_403_FORBIDDEN
-
-
-@deprecated("moved to subscription")
-class QuotaError(QFieldCloudException):
-    """Raised when a quota limitation is hit"""
-
-    code = "over_quota"
-    message = "Quota error"
-    status_code = status.HTTP_402_PAYMENT_REQUIRED
