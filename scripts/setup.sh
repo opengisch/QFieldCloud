9f098886fc61644aaf97fef0b046143686fbafe6
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-01 14:56:57 +0200

    Removed recursive checkout, added missing commands, cleaned up

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-# Init static envars
+# Symlink overrides
+ln -s docker-compose.override.local.yml docker-compose.override.yml
+
+# Check envars
+./check_envvars.sh
+
+# Rewrite .env
 cp .env.example .env
 sed -ri 's/^COMPOSE_FILE=(.*)/COMPOSE_FILE=\1:docker-compose.override.test.yml/g' .env
 eval $(egrep "^[^#;]" .env | xargs -d'\n' -n1 | sed -E 's/(\w+)=(.*)/export \1='"'"'\2'"'"'/g')
@@ -12,8 +18,7 @@ docker images
 # Setup artifacts
 mkdir -m 777 tests_artifacts
 
-# Setup Compose and Django (without building since we"re pulling everything)
+# Setup Compose and Django (without building since we are pulling everything)
 docker compose up -d --no-build
 docker compose exec app python manage.py makemigrations --check
-docker compose exec app python manage.py migrate --noinput
 docker compose exec app python manage.py collectstatic --noinput
