1183ea4e80371bfc099a52db8fd8853053d995a9
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-02 08:24:19 +0200

    typo in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/bash
 
-# Symlink overrides
+echo "Symlink overrides"
 ln -s docker-compose.override.local.yml docker-compose.override.yml
 
-# Check envars
-./check_envvars.sh
+echo "Check envars"
+./scripts/check_envvars.sh
 
-# Rewrite .env
+echo "Rewrite .env"
 cp .env.example .env
 sed -ri 's/^COMPOSE_FILE=(.*)/COMPOSE_FILE=\1:docker-compose.override.test.yml/g' .env
 eval $(egrep "^[^#;]" .env | xargs -d'\n' -n1 | sed -E 's/(\w+)=(.*)/export \1='"'"'\2'"'"'/g')
 
-# Pull, print images
+echo "Pull, print images"
 docker compose pull
 docker images
 
-# Setup artifacts
+echo "Setup artifacts"
 mkdir -m 777 tests_artifacts
 
-# Setup Compose and Django (without building since we are pulling everything)
+echo "Setup Compose and Django (without building since we are pulling everything)"
 docker compose up -d --no-build
 docker compose exec app python manage.py makemigrations --check
 docker compose exec app python manage.py collectstatic --noinput
