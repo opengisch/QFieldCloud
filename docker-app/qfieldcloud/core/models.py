1b1399d1adc9a5dfed7d796acbbdc79076e9ed85
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-22 18:25:41 +0200

    format

--- docker-app/qfieldcloud/core/models.py
+++ docker-app/qfieldcloud/core/models.py
@@ -712,7 +712,7 @@ class Organization(User):
         jobs_created_by_organization_members: QuerySet = (
             Job.objects.filter(
                 project__in=organization_projects_pk,
-                created_at__gte=four_weeks_ago, 
+                created_at__gte=four_weeks_ago,
                 created_at__lte=now,
             )
             .values("created_by", "created_by__username")
@@ -722,7 +722,7 @@ class Organization(User):
         deltas_created_by_organization_members: QuerySet = (
             Delta.objects.filter(
                 project__in=organization_projects_pk,
-                created_at__gte=four_weeks_ago, 
+                created_at__gte=four_weeks_ago,
                 created_at__lte=now,
             )
             .values("created_by", "created_by__username")
