d10991b9e1e92b853f99001c8eecc20385ea2fb5
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-26 11:20:18 +0200

    str not dict

--- docker-app/qfieldcloud/core/tests/test_sentry.py
+++ docker-app/qfieldcloud/core/tests/test_sentry.py
@@ -9,16 +9,16 @@ class QfcTestCase(APITestCase):
     def test_logging_with_sentry(self):
         mock_payload = {
             "name": "request_id_file_name",
-            "pre_serialization": {
-                "data": str(["some", "pre-serialization", "data", "keys"]),
-                "files": str(["some", "files", "to be", "listed"]),
-                "meta": str({"metadata": "of the request pre-serialization"}),
-            },
-            "post_serialization": {
-                "data": str(["some", "post-serialization", "data", "keys"]),
-                "files": str(["some", "files", "to be", "listed"]),
-                "meta": str({"metadata": "of the request post-serialization"}),
-            },
+            "pre_serialization": str({
+                "data": ["some", "pre-serialization", "data", "keys"],
+                "files": ["some", "files", "to be", "listed"],
+                "meta": ["metadata": "of the request pre-serialization"],
+            }),
+            "post_serialization": str({
+                "data": ["some", "post-serialization", "data", "keys"],
+                "files": ["some", "files", "to be", "listed"],
+                "meta": {"metadata": "of the request post-serialization"},
+            }),
             "buffer": StringIO("The traceback of the exception to raise"),
         }
         result = report_serialization_diff_to_sentry(**mock_payload)
