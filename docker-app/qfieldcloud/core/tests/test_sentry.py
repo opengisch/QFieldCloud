f8f844c1303b151ff90f4e0ee0ff821715cf1bd7
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-26 11:43:15 +0200

    format

--- docker-app/qfieldcloud/core/tests/test_sentry.py
+++ docker-app/qfieldcloud/core/tests/test_sentry.py
@@ -9,16 +9,32 @@ class QfcTestCase(APITestCase):
     def test_logging_with_sentry(self):
         mock_payload = {
             "name": "request_id_file_name",
-            "pre_serialization": str({
-                "data": ["some", "pre-serialization", "data", "keys"],
-                "files": ["some", "files", "to be", "listed"],
-                "meta": ["metadata", "of the request pre-serialization"],
-            }),
-            "post_serialization": str({
-                "data": ["some", "post-serialization", "data", "keys"],
-                "files": ["some", "files", "to be", "listed"],
-                "meta": {"metadata": "of the request post-serialization"},
-            }),
+            "pre_serialization": str(
+                {
+                    "data": [
+                        "some",
+                        "pre-serialization",
+                        "data",
+                        "keys",
+                        "日本人 中國的 ~=[]()%+{}@;’#!$_&-  éè  ;∞¥₤€",
+                    ],
+                    "files": ["some", "files", "to be", "listed"],
+                    "meta": ["metadata", "of the request pre-serialization"],
+                }
+            ),
+            "post_serialization": str(
+                {
+                    "data": [
+                        "some",
+                        "post-serialization",
+                        "data",
+                        "keys",
+                        "日本人 中國的 ~=[]()%+{}@;’#!$_&-  éè  ;∞¥₤€",
+                    ],
+                    "files": ["some", "files", "to be", "listed"],
+                    "meta": {"metadata": "of the request post-serialization"},
+                }
+            ),
             "buffer": StringIO("The traceback of the exception to raise"),
         }
         result = report_serialization_diff_to_sentry(**mock_payload)
