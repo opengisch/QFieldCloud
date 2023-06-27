1faf5cf615a99ea1ee86802daf27cf674f3594b7
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-27 10:47:00 +0200

    removing leftover

--- docker-app/qfieldcloud/core/tests/test_sentry.py
+++ docker-app/qfieldcloud/core/tests/test_sentry.py
@@ -1,15 +1,11 @@
 from io import StringIO
 
 from rest_framework.test import APITestCase
-from sentry_sdk import capture_message
 
 from ..utils2.sentry import report_serialization_diff_to_sentry
 
 
 class QfcTestCase(APITestCase):
-    def test_sending_message_to_sentry(self):
-        capture_message("Hello Sentry from test_sentry!")
-
     def test_logging_with_sentry(self):
         mock_payload = {
             "name": "request_id_file_name",
