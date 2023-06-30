55406a651ff0fbecdb23ec05e1a84647cacaf10f
why-not-try-calmer <mrnycticorax@gmail.com>
2023-06-30 10:05:44 +0200

    format

--- docker-app/qfieldcloud/core/migrations/0070_alter_project_is_public.py
+++ docker-app/qfieldcloud/core/migrations/0070_alter_project_is_public.py
@@ -6,13 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0069_auto_20230616_0827'),
+        ("core", "0069_auto_20230616_0827"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='project',
-            name='is_public',
-            field=models.BooleanField(default=False, help_text='Projects marked as public are visible to (but not editable by) anyone.'),
+            model_name="project",
+            name="is_public",
+            field=models.BooleanField(
+                default=False,
+                help_text="Projects marked as public are visible to (but not editable by) anyone.",
+            ),
         ),
     ]
