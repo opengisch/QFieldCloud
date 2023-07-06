591183d65773bfaf29edde4a98d2700695a7e6ee
why-not-try-calmer <mrnycticorax@gmail.com>
2023-07-06 17:54:49 +0200

    type hints

--- docker-app/qfieldcloud/core/pagination.py
+++ docker-app/qfieldcloud/core/pagination.py
@@ -1,15 +1,15 @@
 from rest_framework import pagination, response
 
 
-def parameterize_pagination(cls):
+def parameterize_pagination(_class):
     """
     Set as class attributes the items passed as kwargs.
     """
 
-    def configure_class_object(*args, **kwargs):
+    def configure_class_object(*args, **kwargs) -> type:
         for k, v in kwargs.items():
-            setattr(cls, k, v)
-        return cls
+            setattr(_class, k, v)
+        return _class
 
     return configure_class_object
 
