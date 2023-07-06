07d50ae9a88019c1d5014b94fadde09cc55fa6b9
why-not-try-calmer <mrnycticorax@gmail.com>
2023-07-06 13:59:19 +0200

    typo

--- docker-app/qfieldcloud/core/pagination.py
+++ docker-app/qfieldcloud/core/pagination.py
@@ -3,12 +3,13 @@ from rest_framework import pagination, response
 
 class LimitOffsetPaginationResults(pagination.LimitOffsetPagination):
     """
-    Custom implemention such that response.data = response.data.results
-    For comparison, the DRF's blank implementation defines:
+    Custom implementation such that response.data = (DRF blanket implementation's response).data.results
+    For comparison, the DRF's blanket implementation defines:
+    - response.data.["results"]
     - response.data["count"]
     - response.data["next"]
     - response.data["previous"]
-    - response.data.["results"]"""
+    """
 
     def get_paginated_response(self, data) -> response.Response:
         return response.Response(data)
