4fba42fcffa0a278e140c233dd2b0290cb0d7d91
why-not-try-calmer <mrnycticorax@gmail.com>
2023-08-04 15:33:01 +0200

    only report less than 10 MBs

--- docker-app/qfieldcloud/core/middleware/requests.py
+++ docker-app/qfieldcloud/core/middleware/requests.py
@@ -1,33 +1,28 @@
 import io
 import shutil
+from typing import Any
 
 
 def attach_keys(get_response):
     """
     QF-2540
-    Annotate request with a str representation of relevant fields, so as to obtain a diff
-    by comparing with the post-serialized request later in the callstack.
+    Annotate request with: 
+    - a `str` representation of relevant fields, so as to obtain a diff by comparing with the post-serialized request later in the callstack;
+    - a byte-for-byte, non stealing copy of the raw body to inspect multipart boundaries.
     """
-
     def middleware(request):
+        input_stream = io.BytesIO(request.body)
+
         request_attributes = {
             "file_key": str(request.FILES.keys()),
             "meta": str(request.META),
         }
 
-        content_type = request.headers.get("Content-Type") or request.headers.get(
-            "content-type"
-        )
-
-        # only report raw body multipart requests
-        if content_type and "multipart/form-data;boundary" in content_type:
-            request_attributes["content_type"] = content_type
-            input_stream = io.BytesIO(request.body)
+        # only report raw body of POST requests with a < 10MBs content length
+        if request.META["REQUEST_METHOD"] == "POST" and int(request.META["CONTENT_LENGTH"]) < 10000000:
             output_stream = io.BytesIO()
             shutil.copyfileobj(input_stream, output_stream)
             request.body_stream = output_stream
-
-        if "file" in request.FILES.keys():
             request_attributes["files"] = request.FILES.getlist("file")
 
         request.attached_keys = str(request_attributes)
