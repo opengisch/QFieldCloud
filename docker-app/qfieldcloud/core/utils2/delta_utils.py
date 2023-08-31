1d4560611892ac82e596cf21596e65521196888b
Ivan Ivanov <ivan.ivanov@suricactus.com>
2023-08-31 15:14:01 +0300

    Add a way to easily export deltafiles from Django Admin - Deltas list and Job view

---
+++ docker-app/qfieldcloud/core/utils2/delta_utils.py
@@ -0,0 +1,27 @@
+from typing import Any, Iterable
+from uuid import UUID
+
+
+def generate_deltafile(
+    deltas: Iterable[dict[str, Any]],
+    project_id: UUID,
+    id: UUID = UUID("111111111-1111-1111-1111-11111111111"),
+) -> dict[str, Any]:
+    """Returns a deltafile-structured dictionary with the given deltas.
+    The given deltas must be an iterable with at least one element.
+
+    Args:
+        deltas (Iterable[dict[str, Any]]): deltas in the deltafile
+        project_id (UUID): project ID
+        id (UUID): deltafile UUID
+    """
+    deltas = list(deltas)
+    deltafile = {
+        "deltas": deltas,
+        "files": [],
+        "id": id,
+        "project": project_id,
+        "version": "1.0",
+    }
+
+    return deltafile
