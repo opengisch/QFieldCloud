231bf7bba07c7ac0bda92e1b28fa12b1859bfa30
Ivan Ivanov <ivan.ivanov@suricactus.com>
2023-09-07 01:20:38 +0300

    Do not track `UnsupportedMediaType` exception on Sentry

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -279,6 +279,7 @@ if SENTRY_DSN:
             PlanInsufficientError,
             QuotaError,
         )
+        from rest_framework.exceptions import UnsupportedMediaType
         from rest_framework.exceptions import ValidationError as RestValidationError
 
         ignored_exceptions = (
@@ -288,6 +289,7 @@ if SENTRY_DSN:
             PlanInsufficientError,
             InactiveSubscriptionError,
             RestValidationError,
+            UnsupportedMediaType,
         )
 
         if "exc_info" in hint:
