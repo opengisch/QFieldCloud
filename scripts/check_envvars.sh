bdc7143d89b432359010daa03dd86d3c3d9c7004
stcz <66465573+stcz@users.noreply.github.com>
2024-02-21 14:27:30 +0100

    Fix ENV vars check

--- scripts/check_envvars.sh
+++ scripts/check_envvars.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-python3 scripts/check_envvars.py .env.example --docker-compose-dir . --ignored-varnames CONFIG_PATH DEBUG_DEBUGPY_APP_PORT DEBUG_DEBUGPY_WORKER_WRAPPER_PORT
+python3 scripts/check_envvars.py .env.example --docker-compose-dir . --ignored-varnames CONFIG_PATH DEBUG_DEBUGPY_APP_PORT DEBUG_DEBUGPY_WORKER_WRAPPER_PORT LETSENCRYPT_EMAIL LETSENCRYPT_RSA_KEY_SIZE LETSENCRYPT_STAGING
