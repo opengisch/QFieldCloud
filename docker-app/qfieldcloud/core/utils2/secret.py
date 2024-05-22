c91775c2d55f521eb9906c8e362558b189e53c0b
Michael Schmuki <michael@opengis.ch>
2024-05-22 16:14:45 +0200

    Apply suggestions from code review
    Co-authored-by: Ivan Ivanov <suricactus@users.noreply.github.com>

--- docker-app/qfieldcloud/core/utils2/secret.py
+++ docker-app/qfieldcloud/core/utils2/secret.py
@@ -7,7 +7,8 @@ from django.utils.translation import gettext as _
 PGSERVICE_SECRET_NAME_PREFIX = "PG_SERVICE_"
 
 
-def validate_pg_service_conf(value):
+def validate_pg_service_conf(value: str) -> None:
+    """Checks if a string is a valid `pg_service.conf` file context, otherwise throws a `ValueError`"""
     try:
         buffer = io.StringIO(value)
         config = configparser.ConfigParser()
