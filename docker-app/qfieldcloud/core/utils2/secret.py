6243321ae22b3d6aed8598b7c470d2db4308badc
Michael Schmuki <michael@opengis.ch>
2024-05-22 14:16:02 +0200

    Add validate_pg_service_conf

---
+++ docker-app/qfieldcloud/core/utils2/secret.py
@@ -0,0 +1,23 @@
+import configparser
+import io
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+PGSERVICE_SECRET_NAME_PREFIX = "PG_SERVICE_"
+
+
+def validate_pg_service_conf(value):
+    try:
+        buffer = io.StringIO(value)
+        config = configparser.ConfigParser()
+        config.readfp(buffer)
+
+        if len(config.sections()) != 1:
+            raise ValidationError(
+                _("The `.pg_service.conf` must have exactly one service definition.")
+            )
+    except ValidationError as err:
+        raise err
+    except Exception:
+        raise ValidationError(_("Failed to parse the `.pg_service.conf` file."))
