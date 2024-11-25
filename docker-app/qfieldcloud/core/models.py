43ee4c3351912bf8c9bc731d69c62bca0261a4e6
Thanassis Drivas <thanasisdrivas@gmail.com>
2024-11-25 14:13:07 +0200

    Fix quotes in static method for Team model

--- docker-app/qfieldcloud/core/models.py
+++ docker-app/qfieldcloud/core/models.py
@@ -827,7 +827,7 @@ class Team(User):
     def format_team_name(organization_name, team_name):
         if not team_name:
             raise ValueError("Team name is required.")
-        return f'@{organization_name}/{team_name}
+        return f'@{organization_name}/{team_name}'
 
 class TeamMember(models.Model):
     class Roles(models.TextChoices):
