9ae5d460ac68f319cc99a25f426f9c74e95961d6
gounux <contact@guilhemallaman.net>
2024-12-18 14:48:39 +0100

    Apply code formatting using pre-commit hooks

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -136,7 +136,7 @@ MIDDLEWARE = [
     "qfieldcloud.core.middleware.timezone.TimezoneMiddleware",
     "qfieldcloud.core.middleware.test.TestMiddleware",
     "axes.middleware.AxesMiddleware",
-    "allauth.account.middleware.AccountMiddleware"
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 CRON_CLASSES = [
