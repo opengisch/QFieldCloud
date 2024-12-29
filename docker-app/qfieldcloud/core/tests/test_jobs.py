258bc0e12d07e92e52070707e8cf508e57f3feb0
gounux <contact@guilhemallaman.net>
2024-12-29 09:08:31 +0100

    Split badlayerhandler parsing values test for different job types

--- docker-app/qfieldcloud/core/tests/test_jobs.py
+++ docker-app/qfieldcloud/core/tests/test_jobs.py
@@ -9,6 +9,7 @@ from qfieldcloud.core.models import (
     Project,
 )
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.test import APITransactionTestCase
 
 from .utils import (
@@ -36,24 +37,38 @@ class QfcTestCase(APITransactionTestCase):
         # Create a project
         self.p1 = Project.objects.create(name="p1", is_public=False, owner=self.u1)
 
-    def test_create_job_bad_layer_handler_extracted_values(self):
-        # Test that BadLayerHandler is parsing data properly during package and process projectfile jobs
+    def upload_file(self, project_id: str, local: str, remote: str) -> Response:
+        """Upload a file to QFieldCloud using API.
+
+        Args:
+            project_id (str): id of the project that should contain the file.
+            local (str): name of the local file to upload, should be in `testdata` folder.
+            remote (str): name of the uploaded file.
+
+        Returns:
+            Response: response to the POST HTTP request.
+        """
+        file_path = testdata_path(local)
+        response = self.client.post(
+            f"/api/v1/files/{project_id}/{remote}/",
+            {
+                "file": open(file_path, "rb"),
+            },
+            format="multipart",
+        )
+        return response
+
+    def test_bad_layer_handler_values_for_process_projectfile_job(self):
+        # Test that BadLayerHandler is parsing data properly during process projectfile jobs
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.t1.key)
 
         # Push the QGIS project and datasource GPKG files
-        for local_filename, upload_filename in [
-            ("bumblebees.gpkg", "bumblebees.gpkg"),
-            ("simple_bumblebees_wrong_localized.qgs", "project.qgs"),
-        ]:
-            file_path = testdata_path(local_filename)
-            response = self.client.post(
-                f"/api/v1/files/{self.p1.id}/{upload_filename}/",
-                {
-                    "file": open(file_path, "rb"),
-                },
-                format="multipart",
-            )
-            self.assertTrue(status.is_success(response.status_code))
+        response = self.upload_file(self.p1.id, "bumblebees.gpkg", "bumblebees.gpkg")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.upload_file(
+            self.p1.id, "simple_bumblebees_wrong_localized.qgs", "project.qgs"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Create ProcessProjectfileJob and wait for it to finish
         processprojectfile_job = ProcessProjectfileJob.objects.create(
@@ -70,6 +85,42 @@ class QfcTestCase(APITransactionTestCase):
 
         self.p1.refresh_from_db()
         self.assertIsNotNone(self.p1.project_details)
+
+        # extract layer data from job
+        for step in processprojectfile_job.feedback["steps"]:
+            if step["id"] == "project_details":
+                processfile_layers = step["returns"]["project_details"]["layers_by_id"]
+
+        # "valid" localized layer -> QFC considers it as invalid
+        valid_localized_layer = processfile_layers[VALID_LOCALIZED_LAYER_KEY]
+        self.assertFalse(valid_localized_layer["is_valid"])
+        self.assertTrue(valid_localized_layer["is_localized"])
+
+        # invalid localized layer
+        invalid_localized_layer = processfile_layers[INVALID_LOCALIZED_LAYER_KEY]
+        self.assertFalse(invalid_localized_layer["is_valid"])
+        self.assertTrue(invalid_localized_layer["is_localized"])
+        self.assertEquals(
+            invalid_localized_layer["error_code"], "localized_dataprovider"
+        )
+
+        # invalid layer (datasource does not exist)
+        invalid_layer = processfile_layers[INVALID_LAYER_KEY]
+        self.assertFalse(invalid_layer["is_valid"])
+        self.assertFalse(invalid_layer["is_localized"])
+        self.assertEquals(invalid_layer["error_code"], "invalid_dataprovider")
+
+    def test_bad_layer_handler_values_for_package_job(self):
+        # Test that BadLayerHandler is parsing data properly during package jobs
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.t1.key)
+
+        # Push the QGIS project and datasource GPKG files
+        response = self.upload_file(self.p1.id, "bumblebees.gpkg", "bumblebees.gpkg")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.upload_file(
+            self.p1.id, "simple_bumblebees_wrong_localized.qgs", "project.qgs"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Create Package job and wait for it to finish
         set_subscription(
@@ -91,17 +142,14 @@ class QfcTestCase(APITransactionTestCase):
         self.assertEqual(package_job.status, Job.Status.FINISHED)
         self.assertIsNotNone(package_job.feedback)
 
-        # extract layer data from both jobs
-        for step in processprojectfile_job.feedback["steps"]:
-            if step["id"] == "project_details":
-                processfile_layers = step["returns"]["project_details"]["layers_by_id"]
+        # extract layer data from job
         for step in package_job.feedback["steps"]:
             if step["id"] == "qgis_layers_data":
                 qgis_layers = step["returns"]["layers_by_id"]
             if step["id"] == "qfield_layer_data":
                 qfield_layers = step["returns"]["layers_by_id"]
 
-        for layers in [processfile_layers, qgis_layers, qfield_layers]:
+        for layers in [qgis_layers, qfield_layers]:
             # "valid" localized layer -> QFC considers it as invalid
             valid_localized_layer = layers[VALID_LOCALIZED_LAYER_KEY]
             self.assertFalse(valid_localized_layer["is_valid"])
@@ -116,14 +164,10 @@ class QfcTestCase(APITransactionTestCase):
             )
 
         # invalid layer is not present in qfield layers
-        assert INVALID_LAYER_KEY not in qfield_layers
+        self.assertNotIn(INVALID_LAYER_KEY, qfield_layers)
 
-        for layers in [
-            processfile_layers,
-            qgis_layers,
-        ]:
-            # invalid layer (datasource does not exist)
-            invalid_layer = layers[INVALID_LAYER_KEY]
-            self.assertFalse(invalid_layer["is_valid"])
-            self.assertFalse(invalid_layer["is_localized"])
-            self.assertEquals(invalid_layer["error_code"], "invalid_dataprovider")
+        # invalid layer (datasource does not exist)
+        invalid_layer = qgis_layers[INVALID_LAYER_KEY]
+        self.assertFalse(invalid_layer["is_valid"])
+        self.assertFalse(invalid_layer["is_localized"])
+        self.assertEquals(invalid_layer["error_code"], "invalid_dataprovider")
