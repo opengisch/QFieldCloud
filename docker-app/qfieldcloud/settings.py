87ba9c945c300763e1b11ac7cd74e08a539d8716
gounux <contact@guilhemallaman.net>
2025-01-08 19:17:54 +0100

    Bump `django-axes` to v7.0.1
    Changelog: https://github.com/jazzband/django-axes/blob/master/CHANGES.rst#701-2024-12-02
    
    Breaking changes: https://github.com/jazzband/django-axes/blob/4e89d72b92db044ff3f6b23ea2ab2e681211c98e/docs/2_installation.rst#version-7-breaking-changes-and-upgrading-from-django-axes-version-6

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -372,10 +372,10 @@ ACCOUNT_LOGOUT_ON_GET = True
 ###########################
 # The integer number of login attempts allowed before a record is created for the failed logins. Default: 3
 AXES_FAILURE_LIMIT = 5
-# If True, only lock based on username, and never lock based on IP if attempts exceed the limit. Otherwise utilize the existing IP and user locking logic. Default: False
-AXES_ONLY_USER_FAILURES = True
+# Configures the limiter to handle username only (see https://github.com/jazzband/django-axes/blob/4e89d72b92db044ff3f6b23ea2ab2e681211c98e/docs/2_installation.rst#version-6-breaking-changes-and-upgrading-from-django-axes-version-5)
+AXES_LOCKOUT_PARAMETERS = ["username"]
 # If set, defines a period of inactivity after which old failed login attempts will be cleared. If an integer, will be interpreted as a number of hours. Default: None
-AXES_COOLOFF_TIME = timedelta(minutes=30)
+AXES_COOLOFF_TIME = lambda request: timedelta(minutes=30)  # noqa: E731
 # If True, a successful login will reset the number of failed logins. Default: False
 AXES_RESET_ON_SUCCESS = True
 
