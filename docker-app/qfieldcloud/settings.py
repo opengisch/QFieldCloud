1571f3414304596424bf4416b290b8b356e29332
gounux <contact@guilhemallaman.net>
2025-01-14 15:06:16 +0100

    Set up transifex using tx cli
    Add django en and es po files
    
    Activate i18n and es language in django settings
    
    Add transifex pull and push gh workflows
    
    Edit pull workflow trigger conditions
    
    Remove `minimum_perc` from tx config
    
    Rename es to Spanish in Django language settings
    
    Remove minimum-perc when pulling translations from transifex
    
    Remove `makemessages` from push workflow
    
    Compile messages during pull workflow
    
    Variabilize `USE_I18N` and `LANGUAGE_CODE` in .env file
    
    Keep only en and es in settings languages
    
    Export the env variables file in pull workflow
    
    Update docker-app/qfieldcloud/settings.py
    
    Co-authored-by: Ivan Ivanov <suricactus@users.noreply.github.com>
    
    Update .env.example
    
    Co-authored-by: Ivan Ivanov <suricactus@users.noreply.github.com>
    
    Git-ignore compiled translations
    
    Add compile translations command in `README.md`
    
    Checkout repo as Duke-Nyuki when pulling translations
    
    Finalize translations push and pull CI workflows
    
    Add more CI verbosity
    
    Update .github/workflows/translations_push.yml
    
    Co-authored-by: Ivan Ivanov <suricactus@users.noreply.github.com>
    
    Set unique GH workflow that syncs translations with Transifex

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -220,21 +220,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = os.environ.get("QFIELDCLOUD_DEFAULT_LANGUAGE") or "en"
 
 TIME_ZONE = os.environ.get("QFIELDCLOUD_DEFAULT_TIME_ZONE") or "Europe/Zurich"
 
-USE_I18N = False
-
+USE_I18N = bool(os.environ.get("QFIELDCLOUD_USE_I18N")) or True
 
 USE_TZ = True
 
 
 LANGUAGES = [
-    ("de", "German"),
     ("en", "English"),
-    ("fr", "French"),
-    ("it", "Italian"),
+    ("es", "Spanish"),
 ]
 
 # Static files (CSS, JavaScript, Images)
