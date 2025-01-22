36acea20f4c1f8e3d674f1086c3d052ed8403eae
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-01-22 07:09:22 +0200

    Add documentation and improve typing for  and

--- docker-app/qfieldcloud/core/fields.py
+++ docker-app/qfieldcloud/core/fields.py
@@ -1,6 +1,26 @@
+from __future__ import annotations
+
+from typing import Protocol, cast, Any
 from django.db import models
 from django.db.models.fields.files import FieldFile
 from django.core.files.storage import storages
+
+
+class FileStorageNameModelProtocol(Protocol):
+    def _get_file_storage_name(self) -> str: ...
+
+
+class FileStorageNameModelProtocolMetaclass(type(models.Model), type(Protocol)):  # type: ignore
+    """Required to prevent metaclass error: Metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases"""
+
+
+class ModelWithDynamicStorage(
+    models.Model,
+    FileStorageNameModelProtocol,
+    metaclass=FileStorageNameModelProtocolMetaclass,
+):
+    class Meta:
+        abstract = True
 
 
 class EmptyStorageNameError(Exception):
@@ -10,7 +30,19 @@ class EmptyStorageNameError(Exception):
 
 
 class DynamicStorageFieldFile(FieldFile):
-    def __init__(self, instance, field, name):
+    """The file with a dynamic storage - it is determined by the instance of the model.
+
+    Read more: https://docs.djangoproject.com/en/5.1/ref/models/fields/#filefield-and-fieldfile
+
+    See also:
+        - DynamicStorageFileField
+    """
+
+    def __init__(
+        self, instance: models.Model, field: DynamicStorageFileField, name: str
+    ):
+        instance = cast(ModelWithDynamicStorage, instance)
+
         super().__init__(instance, field, name)
 
         storage_name = instance._get_file_storage_name()
@@ -21,18 +53,28 @@ class DynamicStorageFieldFile(FieldFile):
 
 
 class DynamicStorageFileField(models.FileField):
+    """The field for storing files with a dynamic storage - it is determined by the instance of the model.
+
+    Read more: https://docs.djangoproject.com/en/5.1/ref/models/fields/#filefield-and-fieldfile
+
+    See also:
+        - DynamicStorageFieldFile
+    """
+
     attr_class = DynamicStorageFieldFile
 
-    def pre_save(self, instance, add):
-        storage_name = instance._get_file_storage_name()
+    def pre_save(self, model_instance: models.Model, add: bool) -> Any:
+        model_instance = cast(ModelWithDynamicStorage, model_instance)
+
+        storage_name = model_instance._get_file_storage_name()
 
         if not storage_name:
-            raise EmptyStorageNameError(instance=instance)
+            raise EmptyStorageNameError(instance=model_instance)
 
         storage = storages[storage_name]
 
         self.storage = storage
 
-        file = super().pre_save(instance, add)
+        file = super().pre_save(model_instance, add)
 
         return file
