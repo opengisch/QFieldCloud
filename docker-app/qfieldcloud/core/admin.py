2f8587e716882273f8f29532b358bd400b9dadd9
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-02-21 10:24:03 +0200

    Reorder Project admin tabs: first secrets and then collaborators
    This matches the logic for organizations, where members and teams are
    in the end.

--- docker-app/qfieldcloud/core/admin.py
+++ docker-app/qfieldcloud/core/admin.py
@@ -815,7 +815,10 @@ class ProjectAdmin(QFieldCloudModelAdmin):
         "data_last_packaged_at",
         "project_details__pre",
     )
-    inlines = (ProjectCollaboratorInline, ProjectSecretInline)
+    inlines = (
+        ProjectSecretInline,
+        ProjectCollaboratorInline,
+    )
     search_fields = (
         "id",
         "name__icontains",
