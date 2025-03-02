f5ed45a7c6f00e8b9c85dfcb85705252227e2c9e
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-03-02 23:42:44 +0200

    Add some extra default settings to the cache configuration
    Hopefully should fix all the `KeyError` and `OSError` exceptions.

--- docker-app/qfieldcloud/settings.py
+++ docker-app/qfieldcloud/settings.py
@@ -73,6 +73,12 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
         "LOCATION": "memcached:11211",
+        "OPTIONS": {
+            "no_delay": True,
+            "ignore_exc": True,
+            "max_pool_size": 10,
+            "use_pooling": True,
+        },
     }
 }
 
