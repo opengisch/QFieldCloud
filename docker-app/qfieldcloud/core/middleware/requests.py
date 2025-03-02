2388cf5e320fcd1e36aeb6fbd2d2b7213f961a21
tdrivas <thanasisdrivas@gmail.com>
2025-03-03 00:38:35 +0200

    Fix TypeError in middleware by casting SENTRY_REQUEST_MAX_SIZE_TO_SEND as an integer

--- docker-app/qfieldcloud/core/middleware/requests.py
+++ docker-app/qfieldcloud/core/middleware/requests.py
@@ -17,20 +17,15 @@ def attach_keys(get_response):
     """
 
     def middleware(request):
-        try:
-            sentry_max_size = int(config.SENTRY_REQUEST_MAX_SIZE_TO_SEND)
-        except (ValueError, TypeError):
-            logger.error(
-                "Invalid SENTRY_REQUEST_MAX_SIZE_TO_SEND value. Defaulting to 0."
-            )
-            sentry_max_size = 0
-
         # add a copy of the request body to the request
         if (
             settings.SENTRY_DSN
             and request.method == "POST"
             and "Content-Length" in request.headers
-            and (int(request.headers["Content-Length"]) < sentry_max_size)
+            and (
+                int(request.headers["Content-Length"])
+                < int(config.SENTRY_REQUEST_MAX_SIZE_TO_SEND)
+            )
         ):
             logger.info("Making a temporary copy for request body.")
 
