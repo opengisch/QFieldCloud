98496d2fe2aa38316a2db8f1cde1439707e33b07
gounux <contact@guilhemallaman.net>
2025-05-08 08:09:54 +0200

    Make the attachment tests override the `QfcFilesTestCaseMixin`

--- docker-app/qfieldcloud/filestorage/tests/test_attachments.py
+++ docker-app/qfieldcloud/filestorage/tests/test_attachments.py
@@ -3,14 +3,15 @@ import logging
 
 from django.http import FileResponse
 from rest_framework import status
+from rest_framework.test import APITransactionTestCase
 
 from qfieldcloud.authentication.models import AuthToken
 from qfieldcloud.core.models import (
     Person,
     Project,
 )
+from qfieldcloud.core.tests.mixins import QfcFilesTestCaseMixin
 from qfieldcloud.core.tests.utils import (
-    QfcFilesTestCase,
     setup_subscription_plans,
     testdata_path,
 )
@@ -18,7 +19,7 @@ from qfieldcloud.core.tests.utils import (
 logging.disable(logging.CRITICAL)
 
 
-class QfcTestCase(QfcFilesTestCase):
+class QfcTestCase(QfcFilesTestCaseMixin, APITransactionTestCase):
     def setUp(self):
         setup_subscription_plans()
 
