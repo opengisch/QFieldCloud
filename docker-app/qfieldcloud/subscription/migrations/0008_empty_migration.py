ff4b325ede9c952692a5af0a298e817734268c14
tdrivas <thanasisdrivas@gmail.com>
2025-07-11 08:29:46 +0300

    refactor(subscription): introduce max_organization_members also for subscription

---
+++ docker-app/qfieldcloud/subscription/migrations/0008_empty_migration.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("subscription", "0007_plan_is_seat_flexible_and_more"),
+    ]
+
+    operations = []
