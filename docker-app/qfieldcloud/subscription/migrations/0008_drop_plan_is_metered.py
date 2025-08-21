b9a831f90af8c961d908b547b19185dfc27b1fae
tdrivas <thanasisdrivas@gmail.com>
2025-08-21 12:33:17 +0300

    refactor(subscription): recreate view after the last migration

--- docker-app/qfieldcloud/subscription/migrations/0008_drop_plan_is_metered.py
+++ docker-app/qfieldcloud/subscription/migrations/0008_drop_plan_is_metered.py
@@ -12,4 +12,15 @@ class Migration(migrations.Migration):
             model_name="plan",
             name="is_metered",
         ),
+        migrations.RunSQL(
+            sql="""
+                DROP VIEW IF EXISTS current_subscriptions_vw;
+                CREATE VIEW current_subscriptions_vw AS
+                SELECT
+                    *
+                FROM subscription_subscription
+                WHERE active_since < now()
+                  AND (active_until IS NULL OR active_until > now());
+            """,
+        ),
     ]
