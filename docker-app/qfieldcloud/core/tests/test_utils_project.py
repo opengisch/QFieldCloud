6f5383f1487b5e2676013061d71d65cad5e9b147
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-10-21 05:42:14 +0300

    feat: add support for virtual layers without embedded layers
    Since the virtual and memory layers do not have filenames, we should not
    treat them as regular postgres or WFS layers when checking for `Project.
    has_online_vector_data`.
    
    On the other hand, the virtual layers may embed a postgresql layer and
    we should disable this workaround when a `Plan.is_external_db_supported`
    is `True`.
    
    We take a very radical approach. Instead of looking at the current
    embedded layer provider, we just say - if there are any embedded layers
    (which may be file based and part of the project already), we do assume
    the layer has online vector data and therefore external db.

---
+++ docker-app/qfieldcloud/core/tests/test_utils_project.py
@@ -0,0 +1,38 @@
+# from unittest import TestCase
+import logging
+
+from django.test import TestCase
+
+from qfieldcloud.core.utils2.project import (
+    is_virtual_layer_with_embedded_layers,
+)
+
+logging.disable(logging.CRITICAL)
+
+
+class QfcTestCase(TestCase):
+    def test_is_virtual_layer_without_embedded_layers(self):
+        source = "query=SELECT%20*%20FROM%20points"
+
+        self.assertFalse(is_virtual_layer_with_embedded_layers(source))
+
+    def test_is_virtual_layer_with_embedded_layers(self):
+        source_with_embedded_layers = (
+            "layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
+            "query=SELECT%20*%20FROM%20points"
+        )
+
+        self.assertTrue(
+            is_virtual_layer_with_embedded_layers(source_with_embedded_layers)
+        )
+
+    def test_is_virtual_layer_with_many_embedded_layers(self):
+        source_with_embedded_layers = (
+            "?layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
+            "layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
+            "query=SELECT%20*%20FROM%20points"
+        )
+
+        self.assertTrue(
+            is_virtual_layer_with_embedded_layers(source_with_embedded_layers)
+        )
