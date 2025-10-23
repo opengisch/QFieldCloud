6d5a5d9bbb52c1c17022f1266a6f5a3de5c14a03
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-10-23 05:26:45 +0300

    tests: fix tests for `is_virtual_layer_with_embedded_layers`

--- docker-app/qfieldcloud/core/tests/test_utils_project.py
+++ docker-app/qfieldcloud/core/tests/test_utils_project.py
@@ -11,14 +11,22 @@ logging.disable(logging.CRITICAL)
 
 
 class QfcTestCase(TestCase):
+    def test_is_virtual_layer_without_query_question_mark(self):
+        source = (
+            "layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
+            "query=SELECT%20*%20FROM%20points"
+        )
+
+        self.assertFalse(is_virtual_layer_with_embedded_layers(source))
+
     def test_is_virtual_layer_without_embedded_layers(self):
-        source = "query=SELECT%20*%20FROM%20points"
+        source = "?query=SELECT%20*%20FROM%20points"
 
         self.assertFalse(is_virtual_layer_with_embedded_layers(source))
 
     def test_is_virtual_layer_with_embedded_layers(self):
         source_with_embedded_layers = (
-            "layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
+            "?layer=ogr:%2Fhome%2Fuser%2Ftestdata.gpkg%7Clayername%3Dpoints_xy:points_xy:&"
             "query=SELECT%20*%20FROM%20points"
         )
 
