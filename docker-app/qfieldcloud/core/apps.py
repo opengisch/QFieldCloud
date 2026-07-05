3bec900503b020db7390c5d968a37759639509af
Lukas Graf <lukas@opengis.ch>
2026-07-05 14:00:08 +0200

    Monkey patch dj.forms.formsets.DEFAULT_MAX_NUM to fix saving of large forms:
    Monkey patch django.forms.formsets.DEFAULT_MAX_NUM to a higher value to prevent
    saving of large formsets from erroring out. This monkey patch should be removed
    once this is configurable as a setting in Django.

--- docker-app/qfieldcloud/core/apps.py
+++ docker-app/qfieldcloud/core/apps.py
@@ -6,3 +6,11 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         from qfieldcloud.core import signals  # noqa
+
+        # Monkey patch DEFAULT_MAX_NUM to a higher value to prevent saving
+        # of large formsets from erroring out. This monkey patch should be
+        # removed once this is configurable as a setting in Django.
+        # See QF-8530.
+        import django.forms.formsets
+
+        django.forms.formsets.DEFAULT_MAX_NUM = 5000
