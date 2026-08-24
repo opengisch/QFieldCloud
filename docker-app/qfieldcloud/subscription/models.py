3a2f61ac3c665a6120eb6ff7fd8578c38a450026
Ivan Ivanov <ivan.ivanov@suricactus.com>
2026-08-24 08:24:00 +0300

    ruff: D3XX

--- docker-app/qfieldcloud/subscription/models.py
+++ docker-app/qfieldcloud/subscription/models.py
@@ -731,7 +731,7 @@ class AbstractSubscription(models.Model):
 
     @property
     def active_users(self):
-        "Returns the queryset of active users for running stripe billing period or None."
+        """Returns the queryset of active users for running stripe billing period or None."""
         if not self.account.user.is_organization:
             return None
 
