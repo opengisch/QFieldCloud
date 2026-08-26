e0028576a7a7402f263421f0c2976576e1b7798a
manylon <lyubomirradkov@gmail.com>
2026-08-26 15:42:46 +0300

    fix: use user's saved timezone instead of always falling back to server default
    - fix the check in `TimezoneMiddleware`  to `hasattr(request.user, "useraccount")` and add tests

--- docker-app/qfieldcloud/core/middleware/timezone.py
+++ docker-app/qfieldcloud/core/middleware/timezone.py
@@ -9,9 +9,7 @@ class TimezoneMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated and hasattr(
-            request.user.useraccount, "useraccount"
-        ):
+        if request.user.is_authenticated and hasattr(request.user, "useraccount"):
             user_tz = request.user.useraccount.timezone
         elif settings.TIME_ZONE:
             user_tz = zoneinfo.ZoneInfo(settings.TIME_ZONE)
