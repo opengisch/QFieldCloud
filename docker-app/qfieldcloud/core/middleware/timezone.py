35c360ba2f3d4a375ab174485fc0b673e0914b99
manylon <lyubomirradkov@gmail.com>
2026-08-26 16:24:55 +0300

    feat: add assertion on `UserAccount.timezone` not empty

--- docker-app/qfieldcloud/core/middleware/timezone.py
+++ docker-app/qfieldcloud/core/middleware/timezone.py
@@ -11,6 +11,7 @@ class TimezoneMiddleware:
     def __call__(self, request):
         if request.user.is_authenticated and hasattr(request.user, "useraccount"):
             user_tz = request.user.useraccount.timezone
+            assert user_tz, "UserAccount.timezone must not be empty"
         elif settings.TIME_ZONE:
             user_tz = zoneinfo.ZoneInfo(settings.TIME_ZONE)
         else:
