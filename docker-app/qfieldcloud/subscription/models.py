b85f9bf2f4c10689e86af0049f8346acb8528a90
manylon <lyubomirradkov@gmail.com>
2026-08-28 12:42:43 +0300

    fix: update active users retrieval to use organization context

--- docker-app/qfieldcloud/subscription/models.py
+++ docker-app/qfieldcloud/subscription/models.py
@@ -732,7 +732,7 @@ class AbstractSubscription(models.Model):
             return None
 
         if self.has_current_period:
-            return self.account.user.active_users(
+            return self.account.user.organization.active_users(
                 self.current_period_since,
                 self.current_period_until,
             )
